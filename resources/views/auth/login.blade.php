3918dd3f835bfd88bc1c6044db1778470444c1f2
bendaryy <mohamedbendary1994@gmail.com>
2024-08-10 12:51:33 +0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,12 +8,14 @@
                 <div class="col-md-5">
                     <div class="sign-up-left-content">
                         <div class="sign-up-top-logo">
-                            <a href="{{ route('main.index') }}"><img src="{{getImageFile(get_option('app_logo'))}}" alt="logo"></a>
+                            <a href="{{ route('main.index') }}"><img src="{{ getImageFile(get_option('app_logo')) }}"
+                                    alt="logo"></a>
                         </div>
                         <p>{{ __(get_option('sign_up_left_text')) }}</p>
-                        @if(get_option('sign_up_left_image'))
+                        @if (get_option('sign_up_left_image'))
                             <div class="sign-up-bottom-img">
-                                <img src="{{getImageFile(get_option('sign_up_left_image'))}}" alt="hero" class="img-fluid">
+                                <img src="{{ getImageFile(get_option('sign_up_left_image')) }}" alt="hero"
+                                    class="img-fluid">
                             </div>
                         @endif
                     </div>
@@ -23,43 +25,55 @@
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <h5 class="mb-1">{{__('Sign In')}}</h5>
-                            <p class="font-14 mb-30">{{__('New User')}} ? <a href="{{route('sign-up')}}" class="color-hover text-decoration-underline font-medium">{{__('Create an Account')}}</a></p>
+                            <h5 class="mb-1">{{ __('Sign In') }}</h5>
+                            <p class="font-14 mb-30">{{ __('New User') }} ? <a href="{{ route('sign-up') }}"
+                                    class="color-hover text-decoration-underline font-medium">{{ __('Create an Account') }}</a>
+                            </p>
 
                             <div class="row mb-30">
                                 <div class="col-md-12">
-                                    <label class="label-text-title color-heading font-medium font-16 mb-3">{{__('Email or Phone')}}</label>
-                                    <input type="text" name="email" value="{{old('email')}}" class="form-control email" placeholder="{{ __('Type your email or phone number') }}">
+                                    <label
+                                        class="label-text-title color-heading font-medium font-16 mb-3">{{ __('Email or Phone') }}</label>
+                                    <input type="text" name="email" value="{{ old('email') }}"
+                                        class="form-control email"
+                                        placeholder="{{ __('Type your email or phone number') }}">
                                     @if ($errors->has('email'))
-                                        <span class="text-danger"><i class="fas fa-exclamation-triangle"></i> {{ $errors->first('email') }}</span>
+                                        <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
+                                            {{ $errors->first('email') }}</span>
                                     @endif
                                 </div>
                             </div>
                             <div class="row mb-30">
                                 <div class="col-md-12">
-                                    <label class="label-text-title color-heading font-medium font-16 mb-3">{{__('Password')}}</label>
+                                    <label
+                                        class="label-text-title color-heading font-medium font-16 mb-3">{{ __('Password') }}</label>
                                     <div class="form-group mb-0 position-relative">
-                                        <input class="form-control password" name="password" value="{{old('password')}}" placeholder="*********" type="password">
+                                        <input class="form-control password" name="password" value="{{ old('password') }}"
+                                            placeholder="*********" type="password">
                                         <span class="toggle cursor fas fa-eye pass-icon"></span>
                                     </div>
 
                                     @if ($errors->has('password'))
-                                        <span class="text-danger"><i class="fas fa-exclamation-triangle"></i> {{ $errors->first('password') }}</span>
+                                        <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
+                                            {{ $errors->first('password') }}</span>
                                     @endif
                                 </div>
                             </div>
 
                             <div class="row mb-30">
-                                <div class="col-md-12"><a href="{{ route('forget-password') }}" class="color-hover text-decoration-underline font-medium">{{__('Forgot Password')}}?</a></div>
+                                <div class="col-md-12"><a href="{{ route('forget-password') }}"
+                                        class="color-hover text-decoration-underline font-medium">{{ __('Forgot Password') }}?</a>
+                                </div>
                             </div>
                             <div class="row mb-2">
                                 <div class="col-md-12">
-                                    <button type="submit" class="theme-btn theme-button1 theme-button3 font-15 fw-bold w-100">{{__('Sign In')}}</button>
+                                    <button type="submit"
+                                        class="theme-btn theme-button1 theme-button3 font-15 fw-bold w-100">{{ __('Sign In') }}</button>
                                 </div>
                             </div>
 
-                            <div class="social-media-login-wrap">
-                                @if(env('GOOGLE_LOGIN_STATUS') == 1)
+                            {{-- <div class="social-media-login-wrap">
+                                @if (env('GOOGLE_LOGIN_STATUS') == 1)
                                     <div class="row mb-2">
                                         <div class="col-md-12">
                                             <div class="font-15 fw-bold gap-2 google-login theme-btn theme-button1 theme-button3 w-100">
@@ -69,7 +83,7 @@
                                         </div>
                                     </div>
                                 @endif
-                                @if(env('FACEBOOK_LOGIN_STATUS') == 1)
+                                @if (env('FACEBOOK_LOGIN_STATUS') == 1)
                                     <div class="row mb-2">
                                         <div class="col-md-12">
                                             <div class="theme-btn theme-button1 theme-button3 font-15 fw-bold w-100 facebook-login gap-2">
@@ -79,7 +93,7 @@
                                         </div>
                                     </div>
                                 @endif
-                                @if(env('TWITTER_LOGIN_STATUS') == 1)
+                                @if (env('TWITTER_LOGIN_STATUS') == 1)
                                     <div class="row mb-0">
                                         <div class="col-md-12">
                                             <div class="theme-btn theme-button1 theme-button3 font-15 fw-bold w-100 twitter-login gap-2">
@@ -89,7 +103,7 @@
                                         </div>
                                     </div>
                                 @endif
-                                @if(env('LOGIN_HELP') == 'active')
+                                @if (env('LOGIN_HELP') == 'active')
                                     <div class="table-responsive login-info-table mt-3">
                                         <table class="table table-bordered">
                                             <tbody>
@@ -108,7 +122,7 @@
                                         </table>
                                     </div>
                                 @endif
-                            </div>
+                            </div> --}}
 
                         </form>
                     </div>
@@ -122,27 +136,27 @@
 @push('script')
     <script>
         "use strict"
-        $('#adminCredentialShow').on('click', function (){
+        $('#adminCredentialShow').on('click', function() {
             $('.email').val('admin@gmail.com');
             $('.password').val('123456');
         });
 
-        $('#instructorCredentialShow').on('click', function (){
+        $('#instructorCredentialShow').on('click', function() {
             $('.email').val('instructor@gmail.com');
             $('.password').val('123456');
         });
 
-        $('#studentCredentialShow').on('click', function (){
+        $('#studentCredentialShow').on('click', function() {
             $('.email').val('student@gmail.com');
             $('.password').val('123456');
         });
 
-        $('#affiliatorCredentialShow').on('click', function (){
+        $('#affiliatorCredentialShow').on('click', function() {
             $('.email').val('affililator@gmail.com');
             $('.password').val('123456');
         });
 
-        $('#organizationCredentialShow').on('click', function (){
+        $('#organizationCredentialShow').on('click', function() {
             $('.email').val('organization@gmail.com');
             $('.password').val('123456');
         });
